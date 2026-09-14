07d5d23bea805b78b00c0698c1804528d2439b75
ChrisKnoblauch <chris.knoblauch@stonybrook.edu>
2026-09-14 15:23:53 -0400

    push

--- play1_underflow.c
+++ play1_underflow.c
@@ -4,9 +4,10 @@ int main(void) {
     //unsigned char n = 1;
     //unsigned char z = 0;
     //unsigned char result = z-n;
-    char n = 1;
+    char n = 22;
     char z = 0;
     unsigned char result = z-n;
+    printf("Demonstrating Underflow....");
     printf("The unsigned char result of chars %u - %u is: %u\n", z, n, result);
     return 0;
 }
