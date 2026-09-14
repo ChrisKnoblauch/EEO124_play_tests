abfaa5140b4c7f7f34c73a0d51621952283949ba
ChrisKnoblauch <chris.knoblauch@stonybrook.edu>
2026-09-14 14:59:09 -0400

    a couple cngs

---
+++ play.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    //unsigned char n = 1;
+    //unsigned char z = 0;
+    //unsigned char result = z-n;
+    char n = 1;
+    char z = 0;
+    unsigned char result = z-n;
+    printf("The unsigned char result of chars %u - %u is: %u\n", z, n, result);
+    return 0;
+}
