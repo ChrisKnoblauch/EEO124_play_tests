fca214c342facd3fb96ef22f30e7099f820fda45
ChrisKnoblauch <chris.knoblauch@stonybrook.edu>
2026-09-14 15:39:56 -0400

    push

--- play1_underflow.c
+++ play1_underflow.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-    //unsigned char n = 1;
-    //unsigned char z = 0;
-    //unsigned char result = z-n;
-    char n = 22;
+
+    char n = 1;
     char z = 0;
-    unsigned char result = z-n;
+    unsigned char resultU = z-n;
     printf("Demonstrating Underflow....");
-    printf("The unsigned char result of chars %u - %u is: %u\n", z, n, result);
+    printf("The unsigned char result of chars %u - %u is: %u\n", z, n, resultU);
+
+    char m = 256;
+    char y = 97;
+    unsigned char resultO = m+y;
+    printf("Demonstrating Overflow....");
+    printf("The unsigned char result of chars %u + %u is: %u\n", m, y, resultO);
+    
     return 0;
 }
